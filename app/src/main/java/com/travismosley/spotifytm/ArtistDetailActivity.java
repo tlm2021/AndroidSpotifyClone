52de33d7634499a2f77d5de41b5abc11d28dc66c
Travis Mosley <travis@travismosley.com>
2015-10-21 23:41:01 +1100

    Bunch of cleaning to fix bad state on github.

---
+++ app/src/main/java/com/travismosley/spotifytm/ArtistDetailActivity.java
@@ -0,0 +1,13 @@
+package com.travismosley.spotifytm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ArtistDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_detail);
+    }
+}
